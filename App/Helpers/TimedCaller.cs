ecd6cfac8e3df99d09b7caf182ec2187a9575f5f
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-02 13:11:56 +0300

    [Helpers] Определил класс `TimedCaller`
    В методе вызывает переданный action в течение duration с паузой между вызовами

---
+++ App/Helpers/TimedCaller.cs
@@ -0,0 +1,22 @@
+﻿namespace App.Helpers;
+
+public static class TimedCaller
+{
+	private static bool _shouldWork;
+
+	public static void Call(Action action, TimeSpan duration, TimeSpan timeout)
+	{
+		_shouldWork = true;
+		var thread = new Thread(() =>
+		{
+			while (_shouldWork)
+			{
+				action();
+				Thread.Sleep(timeout);
+			}
+		});
+		thread.Start();
+		Thread.Sleep(duration);
+		_shouldWork = false;
+	}
+}
