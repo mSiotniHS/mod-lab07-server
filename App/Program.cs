e379268233554eb3ded6ad0f5d101db1d7a53b0f
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-02 15:19:47 +0300

    [App] Написал пример программы

--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using App.Analysis;
 
-Console.WriteLine("Hello, World!");
+namespace App;
+
+internal static class Program
+{
+	public static void Main()
+	{
+		var cases = new SystemParameters[]
+		{
+			new() {ArrivalRate = 5, ServiceRate = 1, ThreadCount = 5},
+			new() {ArrivalRate = 10, ServiceRate = 1, ThreadCount = 5},
+			new() {ArrivalRate = 2, ServiceRate = 0.5, ThreadCount = 1}
+		};
+
+		var reports = Analyzer.PerformAnalysis(cases, TimeSpan.FromSeconds(30));
+
+		using var writer = new StreamWriter("results.txt", false, Encoding.UTF8);
+
+		foreach (var report in reports)
+		{
+			writer.WriteLine(report);
+			writer.WriteLine("\n-----\n");
+		}
+	}
+}
