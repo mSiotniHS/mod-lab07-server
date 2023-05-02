545b83a6839effc59436c4fb845a2aabf88378fa
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-02 15:19:37 +0300

    [Analysis] Написал класс `Analyzer`
    Проводит анализ систем по заданному множеству параметров

---
+++ App/Analysis/Analyzer.cs
@@ -0,0 +1,47 @@
+﻿using App.Client;
+using App.Helpers;
+
+namespace App.Analysis;
+
+public static class Analyzer
+{
+	public static IEnumerable<string> PerformAnalysis(IReadOnlyList<SystemParameters> parametersSet, TimeSpan duration)
+	{
+		for (var i = 0; i < parametersSet.Count; i++)
+		{
+			var parameters = parametersSet[i];
+
+			var nextRequestTimeout = 1 / parameters.ArrivalRate;
+			var requestResolvingTimespan = 1 / parameters.ServiceRate;
+
+			var server = new Server.Server(parameters.ThreadCount);
+			var client = new Client.Client();
+			client.Subscribe(server.RequestHandler);
+
+			TimedCaller.Call(
+				() => client.SendRequest(new RequestEventArgs
+				{
+					Id = Random.Shared.Next(1, 500),
+					ResolvingTimespan = TimeSpan.FromSeconds(requestResolvingTimespan)
+				}),
+				duration,
+				TimeSpan.FromSeconds(nextRequestTimeout));
+
+			var actualArrivalRate = server.Data.RequestsReceived / duration.TotalSeconds;
+			var actualServiceRate = server.Data.RequestsAccepted / (duration.TotalSeconds * parameters.ThreadCount);
+
+			var theoreticalStats =
+				Calculator.CalculateStats(parameters.ArrivalRate, parameters.ServiceRate, parameters.ThreadCount);
+			var actualStats =
+				Calculator.CalculateStats(actualArrivalRate, actualServiceRate, parameters.ThreadCount);
+
+			yield return Reporter.CreateReport(
+				i + 1,
+				parameters,
+				duration,
+				theoreticalStats,
+				actualStats,
+				server.Data);
+		}
+	}
+}
