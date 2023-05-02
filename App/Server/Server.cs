45cb6bda0130d35b4bdac308c686e1b5660c7e84
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-02 13:09:56 +0300

    [Server] Написал сервер

---
+++ App/Server/Server.cs
@@ -0,0 +1,55 @@
+using App.Client;
+
+namespace App.Server;
+
+public sealed class Server
+{
+	public record CollectedData
+	{
+		public int RequestsReceived { get; set; }
+		public int RequestsAccepted { get; set; }
+		public int RequestsRejected { get; set; }
+	}
+
+	private readonly BusyThread[] _threads;
+	private readonly object _threadLock = new();
+	public CollectedData Data { get; }
+
+	public Server(int threadCount)
+	{
+		_threads = Enumerable
+			.Range(0, threadCount)
+			.Select(_ => new BusyThread())
+			.ToArray();
+		Data = new CollectedData();
+	}
+
+	public void RequestHandler(object? sender, RequestEventArgs args)
+	{
+		lock (_threadLock)
+		{
+			Data.RequestsReceived++;
+
+			for (var i = 0; i < _threads.Length; i++)
+			{
+				var thread = _threads[i];
+
+				if (thread.IsBusy) continue;
+
+				thread.IsBusy = true;
+				thread.Thread = new Thread(() => ResolveRequest(i, args));
+				thread.Thread.Start();
+				Data.RequestsAccepted++;
+				return;
+			}
+
+			Data.RequestsRejected++;
+		}
+	}
+
+	private void ResolveRequest(int threadIdx, RequestEventArgs args)
+	{
+		Thread.Sleep(args.ResolvingTimespan);
+		_threads[threadIdx].IsBusy = false;
+	}
+}
